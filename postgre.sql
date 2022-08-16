a1dd3e4845878108fcd6cec7772e09530145dfab
recepecem <61947822+recepecem@users.noreply.github.com>
2022-08-16 11:08:07 +0300

    Update postgre.sql

--- postgre.sql
+++ postgre.sql
@@ -1,4 +1,4 @@
-SELECT F.title, F.description FROM film;
+SELECT F.title, F.description FROM film F;
 SELECT * FROM film F WHERE (F.length > 60) AND (F.length < 75);
 SELECT * FROM film F WHERE (F.rental_rate = 0.99) AND ((F.replacement_cost = 12.99) OR (F.replacement_cost = 28.99));
 SELECT C.last_name FROM customer C WHERE C.first_name = 'Mary';
