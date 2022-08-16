6b6c656f93a839176e1328e33c5e92ad70b37bbb
recepecem <61947822+recepecem@users.noreply.github.com>
2022-08-16 11:47:41 +0300

    Update postgre.sql

--- postgre.sql
+++ postgre.sql
@@ -11,3 +11,10 @@ SELECT * FROM film F WHERE NOT ((F.length > 50) AND ((F.rental_rate = 2.99) OR (
 SELECT * FROM film F where F.replacement_cost BETWEEN 12.99 AND 16.99;
 SELECT A.first_name, A.last_name FROM actor A WHERE A.first_name IN ('Penelope', 'Nick', 'Ed');
 SELECT * FROM film F WHERE (F.rental_rate IN(0.99, 2.99, 4.99)) AND (F.replacement_cost IN (12.99, 15.99, 28.99));
+
+--ÖDEV 3
+
+SELECT * FROM country C WHERE C.country LIKE 'A%a';
+SELECT * FROM country C WHERE C.country LIKE '____%n';
+SELECT * FROM film F WHERE F.title ILIKE '%t%_';
+SELECT * FROM film F WHERE (F.title LIKE 'C%') AND (F.length > 90) AND (F.rental_rate = 2.99);
