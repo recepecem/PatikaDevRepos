90b8d95765a122b3264f2b5dacfe699100733272
recepecem <61947822+recepecem@users.noreply.github.com>
2022-08-16 10:58:57 +0300

    Create postgre.sql

---
+++ postgre.sql
@@ -0,0 +1,5 @@
+SELECT F.title, F.description FROM film;
+SELECT * FROM film F WHERE (F.length > 60) AND (F.length < 75);
+SELECT * FROM film F WHERE (F.rental_rate = 0.99) AND ((F.replacement_cost = 12.99) OR (F.replacement_cost = 28.99));
+SELECT C.last_name FROM customer C WHERE C.first_name = 'Mary';
+SELECT * FROM film F WHERE NOT ((F.length > 50) AND ((F.rental_rate = 2.99) OR (F.rental_rate = 2.99)));
